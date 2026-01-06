d5a5007e4dfd680c41e67b0f6b6952203d9e2063
Jack Danger <github@jackdanger.com>
2026-01-06 07:28:21 -0800

    I always forget this

--- install-pi.sh
+++ install-pi.sh
@@ -11,5 +11,6 @@ echo "Setting up systemd service..."
 sudo usermod -aG video,input,audio pi
 
 sudo cp ./kiosk.service /etc/systemd/system/kiosk.service
+sudo systemctl daemon-reload
 sudo systemctl enable kiosk.service
 sudo systemctl start kiosk.service
